cb35f043487789f85d2b9a8c4588fbaed39ab5ee
NicoHirsch1 <nico.hirsch.2004@gmail.com>
2022-01-05 02:47:14 +0100

    Add test order

--- backend/src/test/java/at/htl/recipes/control/UserRepositoryTest.java
+++ backend/src/test/java/at/htl/recipes/control/UserRepositoryTest.java
@@ -3,7 +3,10 @@ package at.htl.recipes.control;
 import at.htl.recipes.entity.User;
 import io.quarkus.test.junit.QuarkusTest;
 import org.jboss.logging.Logger;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
@@ -11,6 +14,7 @@ import javax.transaction.Transactional;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserRepositoryTest {
 
     @Inject
@@ -19,10 +23,11 @@ class UserRepositoryTest {
     @Inject
     UserRepository userRepository;
 
-    @Transactional
+    @Order(1)
     @Test
+    @Transactional
     void saveUser() {
-        var user = userRepository.getEntityManager().merge(new User("TestUser1", "testUser1@gmail.com", "testUser1"));
+        var user = userRepository.getEntityManager().merge(new User("TestUser2", "testUser2@gmail.com", "testUser2"));
         LOG.info(user);
         assertThat(user.id).isNotNull();
     }
